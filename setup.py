9b59339d8cb6cf225651168612734c35a4407ac6
Nicola Iarocci <nicola@nicolaiarocci.com>
2015-09-29 09:19:53 +0200

    Bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = [
 
 setup(
     name='Flask-Sentinel',
-    version='0.0.4',
+    version='0.0.5',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='Nicola Iarocci',
